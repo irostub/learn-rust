ebdb61b129d94756114b9f91f5e49853cf3c0fd1
irostub <irostub@gmail.com>
2023-03-20 23:32:43 +0900

    Init commit

---
+++ learn-rust/src/main.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+use std::env;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", gcd(1, 3));
+
+    let mut numbers = Vec::new();
+
+    //skip 1 은 iterator 가 뱉는 첫번째 값인 프로그램의 이름을 건너뛰기 위해 사용
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            // let t : u64 = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
